7cdf35a4893e56977268cdd7f7540b5750f2f714
Nathan Burgess <Nathan.T.Burgess@evergreen.edu>
2022-10-02 22:03:38 -0400

    Add project files.

---
+++ SouthForkWebsite/Pages/Shop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SouthForkWebsite.Pages
+{
+    public class ShopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
